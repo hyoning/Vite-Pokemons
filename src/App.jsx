73e185388a120ec7c80045e28fbc7e57ef393568
김효은 <hyoeun@gmail.com>
2024-01-25 16:29:21 +0900

    포켓몬 카드 만들기

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,21 @@ import PokeCard from './components/PokeCard'
 
 function App() {
 
-  const [pokemons, setPokemons] = useState([])
+  const [pokemons, setPokemons] = useState([]);
 
-  const url = 'https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0'
+  const url = `https://pokeapi.co/api/v2/pokemon/?limit=10&offset=0`
 
   useEffect(() => {
     fetchPokeData();
   }, [])
 
   const fetchPokeData = async () => {
-    try{
+    try {
+     
       const response = await axios.get(url);
-      setPokemons(response.data.results);
-    } catch(error){
+      // console.log(response.data.results);
+      setPokemons([...pokemons, ...response.data.results]);
+    } catch (error) {
       console.error(error);
     }
   }
@@ -25,22 +27,21 @@ function App() {
   return (
     <article className='pt-6'>
       <header className='flex flex-col gap-2 w-full px-4 z-50'>
-
+          Input Form
       </header>
-      <section className='pt-6 flex flex-col items-center overflow-auto justify-center'>
-        <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl'>
-          {pokemons.length > 0 ? (
-            pokemons.map(({url, name}, index) => (
-                <div>
-                  <PokeCard key={url} url={url} name={name}/>
-                </div>
+      <section className='pt-6 flex flex-col justify-center items-center overflow-auto z-0'>
+        <div className='flex flex-row flex-wrap gap-[16px] items-center justify-center px-2 max-w-4xl '>
+          {pokemons.length > 0 ?
+            (
+              pokemons.map(({ url, name }, index) => (
+                <PokeCard key={url} url={url} name={name} />
               ))
-            ) : (
-              <h2 className="font-medium text-lg text-slate-900 mb-1">
-                포켓몬이 없습니다. 
+            ) :
+            (
+              <h2 className='font-medium text-lg text-slate-900 mb-1'>
+                포켓몬이 없습니다.
               </h2>
-            )
-          }
+            )}
         </div>
       </section>
     </article>
